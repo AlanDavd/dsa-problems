e3c12994c29e447ce29004e35ec27d8f7f744c0f
AlanDavd <alandavidrl11@gmail.com>
2021-04-16 15:12:13 -0500

    add clone folder script

---
+++ codeforces/setfol.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# alandavidrl11@gmail.com github.com/AlanDavd
+# Copy and rename setup folder for a problem within the codeforces folder
+# Usage: ./setfol.sh <folder name>
+
+mkdir $1
+cp ./setup/* $1
