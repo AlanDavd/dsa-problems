f74986d130384319198625f676043009fef9e4b3
AlanDavd <alan.rojas@apli.jobs>
2021-12-08 20:36:58 -0600

    add setup for future problems

---
+++ advent-of-code-2021/setup/app.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   int a, b;
+   scanf("%d%d", &a, &b);
+   printf("%d - %d", a, b);
+   return 0;
+}
