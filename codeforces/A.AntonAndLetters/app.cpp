fa096a463e9e21d2485df0898959637271709a4c
AlanDavd <alan.rojas@apli.jobs>
2022-01-04 19:12:33 -0600

    add A.AntonAndLetters cpp solution

--- codeforces/A.AntonAndLetters/app.cpp
+++ codeforces/A.AntonAndLetters/app.cpp
@@ -6,18 +6,15 @@ typedef long long ll;
 int main() {
    ios::sync_with_stdio(0);
    cin.tie(0);
-   char s[1001];
-   int counter = 0;
-   gets(s);
-   sort(s, strlen(s));
-   for (int i = 0; i < strlen(s)-1; i++) {
-      if (s[i] >= 97 && s[i] <= 122) {
-         counter++;
-         if (s[i] == s[i+1]) {
-            counter--;
-         }
-      }
+   string s;
+   set<char> counter;
+
+   getline(cin, s);
+   for (int i = 0; i < s.size(); i++) {
+     if (s[i] >= 97 && s[i] <= 122) {
+       counter.insert(s[i]);
+     }
    }
-   printf("%d\n", counter);
+   cout << counter.size();
    return 0;
 }
